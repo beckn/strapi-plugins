872e024ae7f5749be21d65ebbd6964d34d326adc
shreyvishal <shreya.vishal@eminds.ai>
2024-04-04 18:47:57 +0530

    Added: Keyword Based Search

--- plugins/beckn-bpp-adapter/server/util/filter.util.ts
+++ plugins/beckn-bpp-adapter/server/util/filter.util.ts
@@ -1,47 +1,68 @@
 import { KeyValuePair } from "../types";
 
 export class FilterUtil {
-    static getItemFilter = (item: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: '',
-            name: '',
-            code: ''
-        };
-        filter.id = item.id;
-        filter.name = {
-            $contains: item.descriptor?.name
+  static getItemFilter = (item: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      name: "",
+      code: ""
+    };
+    filter.$or = [];
+    filter.code = item.descriptor?.code;
+    filter.id = item?.id;
+    if (item?.descriptor?.name) {
+      filter.$or.push({
+        name: {
+          $contains: item.descriptor?.name
         }
-        filter.code = item.descriptor?.code;
-        return filter;
+      });
     }
 
-    static getProviderFilter = (provider: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: '',
-            provider_name: ''
-        };
-        filter.id = provider.id;
-        filter.provider_name = {
-            $contains: provider.descriptor?.name
-        };
-        return filter;
-    }
-
-    static getCategoryFilter = (category: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: '',
-            value: '',
-            category_code: '',
-        };
-        filter.id = category.id;
-        filter.value = {
-            $contains: category.descriptor?.name
+    if (item?.descriptor?.name || item?.descriptor?.short_desc) {
+      filter.$or.push({
+        short_desc: {
+          $contains: item.descriptor?.name || item.descriptor?.short_desc
         }
-        filter.category_code = category.descriptor?.code
-        return filter;
+      });
+    }
+    if (item?.descriptor?.name || item?.descriptor?.long_desc) {
+      filter.$or.push({
+        long_desc: {
+          $contains: item.descriptor?.name || item.descriptor?.long_desc
+        }
+      });
     }
 
-    static getItemsFilter = (items) => {
-        return items.map((obj: { id: string }) => obj.id);
-    }
+    return filter;
+  };
+
+  static getProviderFilter = (provider: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      provider_name: ""
+    };
+    filter.id = provider.id;
+    filter.provider_name = {
+      $contains: provider.descriptor?.name
+    };
+    return filter;
+  };
+
+  static getCategoryFilter = (category: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      value: "",
+      category_code: ""
+    };
+    filter.id = category.id;
+    filter.value = {
+      $contains: category.descriptor?.name
+    };
+    filter.category_code = category.descriptor?.code;
+    return filter;
+  };
+
+  static getItemsFilter = (items) => {
+    return items.map((obj: { id: string }) => obj.id);
+  };
 }
