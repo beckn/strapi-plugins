c78210bfd2b702f74a09256ea67780d3dbc816cb
shreyvishal <shreya.vishal@eminds.ai>
2024-04-05 13:15:59 +0530

    Added: Fix_tag_based_search

--- plugins/beckn-bpp-adapter/server/services/search/appointment-workflow-service.ts
+++ plugins/beckn-bpp-adapter/server/services/search/appointment-workflow-service.ts
@@ -1,96 +1,162 @@
-import { Strapi } from '@strapi/strapi';
-import { ObjectUtil, FilterUtil } from '../../util';
-import { KeyValuePair } from '.././../types';
-import { PLUGIN } from '../../constants';
+import { Strapi } from "@strapi/strapi";
+import { ObjectUtil, FilterUtil } from "../../util";
+import { KeyValuePair } from ".././../types";
+import { PLUGIN } from "../../constants";
 
 export default ({ strapi }: { strapi: Strapi }) => ({
-    async index({ message, context }) {
-        const {
-            item,
-            provider,
-            category
-        } = message?.intent || {};
-        const { domain } = context;
-        const filters: KeyValuePair = provider ? FilterUtil.getProviderFilter(provider) : {};
-        const populate: KeyValuePair = {
-            items: {
+  async index({ message, context }) {
+    const { item, provider, category } = message?.intent || {};
+    const { domain } = context;
+    const filters: KeyValuePair = provider
+      ? FilterUtil.getProviderFilter(provider)
+      : {};
+    const populate: KeyValuePair = {
+      items: {
+        populate: {
+          cat_attr_tag_relations: {
+            filters: {
+              taxanomy: {
+                $in: ["TAG", "CATEGORY"]
+              }
+            }
+          },
+          image: {},
+          service: {},
+          sc_retail_product: {
+            populate: {
+              price_bareakup_ids: {},
+              product_cancel: {
                 populate: {
-                    cat_attr_tag_relations: {
-                        filters: {
-                            taxanomy: {
-                                $in: ['TAG', 'CATEGORY']
-                            }
-                        }
-                    },
-                    image: {},
-                    service: {},
-                    sc_retail_product: {
-                        populate: {
-                            price_bareakup_ids: {},
-                            product_cancel: {
-                                populate: {
-                                    cancel_term_id: {}
-                                }
-                            }
-                        }
-                    },
-                    item_fulfillment_ids: {
-                        populate: {
-                            fulfilment_id: {
-                                populate: {
-                                    agent_ids: {}
-                                }
-                            },
-                            location_id: {}
-                        }
+                  cat_attr_tag_relations: {
+                    filters: {
+                      taxanomy: {
+                        $in: ["TAG", "CATEGORY"]
+                      }
                     }
+                  },
+                  image: {},
+                  service: {},
+                  sc_retail_product: {
+                    populate: {
+                      price_bareakup_ids: {},
+                      product_cancel: {
+                        populate: {
+                          cancel_term_id: {}
+                        }
+                      }
+                    }
+                  },
+                  item_fulfillment_ids: {
+                    populate: {
+                      fulfilment_id: {
+                        populate: {
+                          agent_ids: {}
+                        }
+                      },
+                      location_id: {}
+                    }
+                  },
+                  cancel_term_id: {}
                 }
-            },
-            payment_methods: {},
-            category_ids: {},
-            location_id: {},
-            fulfillments: {}
-        }
-
-        if (domain) {
-            filters.domain_id = {
-                DomainName: domain
+              }
             }
-        }
-        if (item) {
-            const itemFilter = ObjectUtil.removeEmptyObjectKeys(FilterUtil.getItemFilter(item));
-            if (Object.keys(itemFilter).length) {
-                populate.items.filters = filters.items = itemFilter
+          },
+          item_fulfillment_id: {
+            populate: {
+              fulfilment_id: {
+                populate: {
+                  agent_ids: {}
+                }
+              },
+              location_id: {}
             }
+          }
         }
+      },
+      payment_methods: {},
+      category_ids: {},
+      location_id: {},
+      fulfillments: {}
+    };
 
-        if (category) {
-            const categoryFilter = ObjectUtil.removeEmptyObjectKeys(FilterUtil.getCategoryFilter(category));
-            if (Object.keys(categoryFilter).length) {
-                populate.category_ids.filters = filters.category_ids = categoryFilter;
-            }
-        }
-        ObjectUtil.removeEmptyObjectKeys(filters);
-        ObjectUtil.removeEmptyKeys(populate);
-        const providers = await strapi.entityService.findMany('api::provider.provider', {
-            filters,
-            populate
-        });
-
-        const commonService = strapi
-            .plugin(PLUGIN)
-            .service('commonService');
-        await Promise.all(providers.map(async (provider) => {
-            await Promise.all(await provider.items.map(async (item) => {
-                await Promise.all(item['cat_attr_tag_relations']?.map(async (taxanomy) => {
-                    if (taxanomy.taxanomy === "CATEGORY") {
-                        taxanomy.taxanomy_id = await commonService.getCategoryById(taxanomy.taxanomy_id);
-                    } else if (taxanomy.taxanomy === "TAG") {
-                        taxanomy.taxanomy_id = await commonService.getTagById(taxanomy.taxanomy_id);
-                    }
-                }));
-            }));
-        }));
-        return providers;
+    if (domain) {
+      filters.domain_id = {
+        DomainName: domain
+      };
     }
+    if (item) {
+      const itemFilter = ObjectUtil.removeEmptyObjectKeys(
+        FilterUtil.getItemFilter(item)
+      );
+      if (Object.keys(itemFilter).length) {
+        populate.items.filters = filters.items = itemFilter;
+      }
+    }
+
+    if (category) {
+      const categoryFilter = ObjectUtil.removeEmptyObjectKeys(
+        FilterUtil.getCategoryFilter(category)
+      );
+      if (Object.keys(categoryFilter).length) {
+        populate.category_ids.filters = filters.category_ids = categoryFilter;
+      }
+    }
+    ObjectUtil.removeEmptyObjectKeys(filters);
+    ObjectUtil.removeEmptyKeys(populate);
+
+    const providers = await strapi.entityService.findMany(
+      "api::provider.provider",
+      {
+        filters,
+        populate
+      }
+    );
+
+    const commonService = strapi.plugin(PLUGIN).service("commonService");
+    await Promise.all(
+      providers.map(async (provider) => {
+        await Promise.all(
+          await provider.items.map(async (item) => {
+            await Promise.all(
+              item["cat_attr_tag_relations"]?.map(async (taxanomy) => {
+                if (taxanomy.taxanomy === "CATEGORY") {
+                  taxanomy.taxanomy_id = await commonService.getCategoryById(
+                    taxanomy.taxanomy_id
+                  );
+                } else if (taxanomy.taxanomy === "TAG") {
+                  taxanomy.taxanomy_id = await commonService.getTagById(
+                    taxanomy.taxanomy_id
+                  );
+                }
+              })
+            );
+          })
+        );
+      })
+    );
+    const newProviders = providers.map((provider: any) => {
+      const filteredItems = provider?.items?.filter((itemFromStrapi) => {
+        const isMatch = item?.tags[0]?.list?.some((listItem: any) =>
+          itemFromStrapi?.cat_attr_tag_relations?.some(
+            (tag: any) =>
+              tag?.taxanomy_id?.code === listItem?.descriptor?.code &&
+              tag?.taxanomy_id?.value === listItem?.value
+          )
+        );
+        if (isMatch) {
+          return itemFromStrapi;
+        }
+      });
+
+      if (filteredItems?.length) {
+        return { ...provider, items: filteredItems };
+      }
+      return null;
+    });
+    if ((newProviders as any[]).every((elem: any) => elem === null)) {
+      return providers;
+    } else {
+      return newProviders;
+    }
+  }
 });
