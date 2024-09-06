7a0dc231a726620087610c4cd577a3f7ebeda99c
Abhishek Y <abhisheky@Abhisheks-MacBook-Pro.local>
2024-09-06 08:48:38 +0530

    Created create policy API

--- plugins/policy-api/server/controllers/policy/policy-controller.ts
+++ plugins/policy-api/server/controllers/policy/policy-controller.ts
@@ -4,7 +4,28 @@ import { PLUGIN } from "../../../contstants";
 export default ({ strapi }: { strapi: Strapi }) => ({
   async createPolicy(ctx) {
     try {
-      console.log('Create policy logic');
+      const { policy = {} } = ctx.request?.body;
+      const { coverage = [] } = policy;
+      // const { coverage = [] } = ctx.request?.body?.policy;
+      // coverage validation
+      // coverage temporal validation
+      // coverage temporal range validation
+      if (
+        coverage.length === 1 &&
+        coverage[0].temporal &&
+        coverage[0].temporal.length === 1 &&
+        coverage[0].temporal[0].range &&
+        new Date(coverage[0].temporal[0].range.start).getTime() > 0 &&
+        new Date(coverage[0].temporal[0].range.end).getTime() > 0
+      ) {
+        const policyService = strapi
+          .plugin(PLUGIN)
+          .service("policyService");
+        const policyResult = await policyService.createPolicy(policy);
+        ctx.body = policyResult;
+      } else {
+        throw new Error("Invalid coverage parameters");
+      }
     } catch (error) {
       ctx.badRequest(error.message);
     }
---
+++ plugins/policy-api/server/services/policy/policy-service.ts
@@ -0,0 +1,66 @@
+import { Strapi } from "@strapi/strapi";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+export default ({ strapi }: { strapi: Strapi }) => ({
+  async createPolicy(policyData: any) {
+    try {
+      const currentDate = new Date();
+      const publishedAt = currentDate.toISOString();
+      const {
+        coverage = [],
+        status,
+        domain,
+        descriptor = {},
+        owner = {},
+        media = {},
+        type,
+        geofences,
+        rules,
+        createdBy,
+      } = policyData;
+
+      const ownerName = owner.descriptor?.name;
+      const ownerEmail = owner.descriptor?.contact?.email;
+      if (!ownerName || !ownerEmail) {
+        throw new Error("Owner details not found");
+      }
+      let ownerData = await strapi.entityService.findMany('api::pp-owner.pp-owner', {
+        filters: { name: ownerName, email: ownerEmail },
+        start: 0,
+        limit: 1
+      })[0];
+      if (!ownerData?.length) {
+        ownerData = await strapi.entityService.create('api::pp-owner.pp-owner', {
+          data: {
+            name: ownerName,
+            email: ownerEmail,
+            publishedAt
+          }
+        });
+
+      }
+      const insertData = {
+        coverage,
+        status,
+        domain,
+        type,
+        rules,
+        pp_owner: ownerData.id,
+        name: descriptor.name,
+        short_description: descriptor.short_desc,
+        geofences,
+        policyId: uuidv4(),
+        mediaMimeType: media.mimetype,
+        mediaUrl: media.url,
+        publishedAt
+        // createdByUser: createdBy ? createdBy : "system"
+      };
+      console.log('abhi', ownerData, insertData);
+      const policy = await strapi.entityService.create('api::pp-policy.pp-policy', { data: insertData });
+      return policy;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+});
