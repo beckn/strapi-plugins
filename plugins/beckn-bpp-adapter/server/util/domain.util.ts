137d0480cfef3b4419ec9b033c579dcc7fbd93d8
Abhishek Y <abhisheky@Abhisheks-MacBook-Pro.local>
2024-09-25 12:47:26 +0530

    Modified API for P2P energy search

--- plugins/beckn-bpp-adapter/server/util/domain.util.ts
+++ plugins/beckn-bpp-adapter/server/util/domain.util.ts
@@ -1,5 +1,6 @@
-import { HOSPITALITY_DOMAINS, TOURISM_DOMAINS, MOBILITY_DOMAINS } from "../constants"
+import { HOSPITALITY_DOMAINS, TOURISM_DOMAINS, MOBILITY_DOMAINS, ENERGY_DOMAINS } from "../constants"
 
 export const isHospitality = (context) => HOSPITALITY_DOMAINS.includes(context?.domain);
 export const isTourism = (context) => TOURISM_DOMAINS.includes(context?.domain);
 export const isMobility = (context) => MOBILITY_DOMAINS.includes(context?.domain);
+export const isEnergy = (context) => ENERGY_DOMAINS.includes(context?.domain);
