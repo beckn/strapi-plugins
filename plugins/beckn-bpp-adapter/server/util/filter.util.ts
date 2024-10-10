feb2b3a9aa7fa459044068a2000f3e9d447935f7
shreyvishal <shreya.vishal@eminds.ai>
2024-10-10 15:48:40 +0530

    Added: Optional Chaining in getSCRetailFilter

--- plugins/beckn-bpp-adapter/server/util/filter.util.ts
+++ plugins/beckn-bpp-adapter/server/util/filter.util.ts
@@ -5,86 +5,86 @@ import { isHospitality, isTourism } from "./domain.util";
 import { isInRange, findStoresAlongRouteWithinDistance } from "./location.util";
 
 export class FilterUtil {
-    static getItemFilter = (item: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: "",
-            name: "",
-            code: "",
-            publishedAt: { $notNull: true }
-        };
-        filter.$or = [];
-        filter.code = item.descriptor?.code;
-        filter.id = item?.id;
-        if (item?.descriptor?.name) {
-            filter.$or.push({
-                name: {
-                    $contains: item.descriptor?.name
-                }
-            });
-        }
-
-        if (item?.descriptor?.name || item?.descriptor?.short_desc) {
-            filter.$or.push({
-                short_desc: {
-                    $contains: item.descriptor?.name || item.descriptor?.short_desc
-                }
-            });
-        }
-        if (item?.descriptor?.name || item?.descriptor?.long_desc) {
-            filter.$or.push({
-                long_desc: {
-                    $contains: item.descriptor?.name || item.descriptor?.long_desc
-                }
-            });
-        }
-
-        return filter;
+  static getItemFilter = (item: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      name: "",
+      code: "",
+      publishedAt: { $notNull: true }
     };
-
-    static getSCRetailFilter = (item) => {
-        const filter: any = {}
-        if (item.quantity?.available?.measure?.value) {
-            const value = item.quantity?.available?.measure?.value;
-            filter.stock_quantity = {
-                $gte: parseInt(value)
-            }
+    filter.$or = [];
+    filter.code = item.descriptor?.code;
+    filter.id = item?.id;
+    if (item?.descriptor?.name) {
+      filter.$or.push({
+        name: {
+          $contains: item.descriptor?.name
         }
-        if (item.price?.maximum_value) {
-            const maximum_value = item.price?.maximum_value;
-            filter.min_price = {
-                $gte: parseInt(maximum_value)
-            }
-        }
-        return filter;
+      });
     }
 
-    static getProviderFilter = (provider: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: "",
-            provider_name: ""
-        };
-        filter.id = provider.id;
-        filter.provider_name = {
-            $contains: provider.descriptor?.name
-        };
-        return filter;
-    };
+    if (item?.descriptor?.name || item?.descriptor?.short_desc) {
+      filter.$or.push({
+        short_desc: {
+          $contains: item.descriptor?.name || item.descriptor?.short_desc
+        }
+      });
+    }
+    if (item?.descriptor?.name || item?.descriptor?.long_desc) {
+      filter.$or.push({
+        long_desc: {
+          $contains: item.descriptor?.name || item.descriptor?.long_desc
+        }
+      });
+    }
 
-    static getCategoryFilter = (category: KeyValuePair = {}) => {
-        const filter: KeyValuePair = {
-            id: "",
-            value: "",
-            category_code: ""
-        };
-        filter.id = category.id;
-        filter.value = {
-            $contains: category.descriptor?.name
-        };
-        filter.category_code = category.descriptor?.code;
-        return filter;
-    };
+    return filter;
+  };
 
-    static getItemsFilter = (items) => {
-        return items.map((obj: { id: string }) => obj.id);
+  static getSCRetailFilter = (item) => {
+    const filter: any = {};
+    if (item?.quantity?.available?.measure?.value) {
+      const value = item?.quantity?.available?.measure?.value;
+      filter.stock_quantity = {
+        $gte: parseInt(value)
+      };
+    }
+    if (item?.price?.maximum_value) {
+      const maximum_value = item.price?.maximum_value;
+      filter.min_price = {
+        $gte: parseInt(maximum_value)
+      };
+    }
+    return filter;
+  };
+
+  static getProviderFilter = (provider: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      provider_name: ""
     };
+    filter.id = provider.id;
+    filter.provider_name = {
+      $contains: provider.descriptor?.name
+    };
+    return filter;
+  };
+
+  static getCategoryFilter = (category: KeyValuePair = {}) => {
+    const filter: KeyValuePair = {
+      id: "",
+      value: "",
+      category_code: ""
+    };
+    filter.id = category.id;
+    filter.value = {
+      $contains: category.descriptor?.name
+    };
+    filter.category_code = category.descriptor?.code;
+    return filter;
+  };
+
+  static getItemsFilter = (items) => {
+    return items.map((obj: { id: string }) => obj.id);
+  };
 }
