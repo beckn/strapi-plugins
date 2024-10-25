640c0cfcb657a8a72b2bd48963f7137fc4d5c5e5
em-abee <abhishek.yadav@eminds.ai>
2024-10-25 13:07:45 +0530

    Add create and get der endpoints - Add logic for adding energy entry for catalog

--- plugins/beckn-trade-bpp/server/controllers/energy/energy-controller.ts
+++ plugins/beckn-trade-bpp/server/controllers/energy/energy-controller.ts
@@ -30,8 +30,31 @@ export default ({ strapi }: { strapi: Strapi }) => ({
             const { userId } = ctx.request.query;
             console.log('Req: ', ctx.request);
             console.log('query: ', ctx.request.query);
-            
             const result = await energyService.getCredential(Number(userId));
+            ctx.body = result;
+        } catch (error) {
+            ctx.badRequest(error.message);
+        }
+    },
+    async createDer(ctx) {
+        try {
+            const { files, body } = ctx.request;
+            console.log(files.proof);
+            const energyService = strapi
+                .plugin("beckn-trade-bpp")
+                .service("energyService");
+            const result = await energyService.createDer(body, files.proof);
+            ctx.body = result;
+        } catch (error) {
+            ctx.badRequest(error.message);
+        }
+    },
+    async getDer(ctx) {
+        try {
+            const energyService = strapi
+                .plugin("beckn-trade-bpp")
+                .service("energyService");
+            const result = await energyService.getDer(ctx.query.id);
             ctx.body = result;
         } catch (error) {
             ctx.badRequest(error.message);
