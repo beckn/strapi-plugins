f46e2ece207ce2e5149535a5dee77c7298c905e2
shreyvishal <shreya.vishal@eminds.ai>
2025-03-06 01:05:56 +0530

    Added: Mobile Based Login

--- plugins/beckn-energy-finance/server/services/auth-service.ts
+++ plugins/beckn-energy-finance/server/services/auth-service.ts
@@ -48,15 +48,13 @@ export default ({ strapi }: { strapi: Strapi }) => ({
   async mobileLogin(loginDto: any) {
     try {
       const { phone } = loginDto;
-      const user = await strapi
-        .query("plugin::users-permissions.user")
-        .findMany({
-          filter: {
-            user: {
-              agent: {
-                agent_profile: {
-                  phone_number: { $eqi: phone }
-                }
+      const user = await strapi.entityService.findMany(
+        "plugin::users-permissions.user",
+        {
+          filters: {
+            agent: {
+              agent_profile: {
+                phone_number: phone
               }
             }
           },
@@ -72,7 +70,8 @@ export default ({ strapi }: { strapi: Strapi }) => ({
               provider: true
             }
           }
-        });
+        }
+      );
       // console.log(JSON.stringify(user));
       if (!user || !user.length) {
         throw new Error("User Not found with Given Mobile Number");
