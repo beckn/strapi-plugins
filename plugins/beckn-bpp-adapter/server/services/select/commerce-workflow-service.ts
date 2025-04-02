4ab45036598728586ee76258c7f32e74df74b43e
shreyvishal <shreya.vishal@eminds.ai>
2025-04-02 14:02:44 +0530

    Added: Dynamic Price Breakup in on_select API

--- plugins/beckn-bpp-adapter/server/services/select/commerce-workflow-service.ts
+++ plugins/beckn-bpp-adapter/server/services/select/commerce-workflow-service.ts
@@ -138,6 +138,11 @@ export default ({ strapi }: { strapi: Strapi }) => ({
       itemDetails[0] = { ...itemDetails[0], ...quantitySelected };
 
       itemDetails = itemDetails.map((provider) => {
+        const providerItemIds = provider?.items.map((item) => String(item.id));
+        // Filter items that exist in provider.items
+        const itemsBody = items.filter((item) =>
+          providerItemIds.includes(String(item.id))
+        );
         provider.items = provider.items.map((responseItem) => {
           // Find the matching item in request body by id
           const bodyItem = items.find(
@@ -156,7 +161,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
           }
           return responseItem;
         });
-
+        provider.itemsBody = itemsBody;
         return provider;
       });
       return itemDetails;
