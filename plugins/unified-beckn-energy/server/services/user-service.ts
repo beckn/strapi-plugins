81f11e6df5d4bd281719ae7f43544324dee01d84
shreyvishal <shreya.vishal@eminds.ai>
2025-04-04 19:35:03 +0530

    Modified Names for Provider Name

--- plugins/unified-beckn-energy/server/services/user-service.ts
+++ plugins/unified-beckn-energy/server/services/user-service.ts
@@ -834,7 +834,10 @@ export default ({ strapi }: { strapi: Strapi }) => ({
     try {
       const providerId = user?.deg_wallet?.provider?.id;
       if (!providerId) {
-        throw new Error("No Provider is linked to this user to get catalogues");
+        return {
+          message: "No Provider is linked to this user to get catalogues"
+        };
+        // throw new Error("No Provider is linked to this user to get catalogues");
       }
       const providerData = await strapi.entityService.findOne(
         "api::provider.provider",
