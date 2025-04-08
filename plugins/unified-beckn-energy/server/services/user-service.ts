6106871eb5848cf47b87c0b266f54ce66c22382d
shreyvishal <shreya.vishal@eminds.ai>
2025-04-08 14:44:50 +0530

    Added:

--- plugins/unified-beckn-energy/server/services/user-service.ts
+++ plugins/unified-beckn-energy/server/services/user-service.ts
@@ -579,11 +579,15 @@ export default ({ strapi }: { strapi: Strapi }) => ({
               .query("api::provider.provider")
               .create({
                 data: {
-                  provider_name:
-                    `${providerNameSuggestions[Math.floor(Math.random() * 4)]}` || providerData.name,
+                  // provider_name:
+                  //   `${user?.agent?.first_name}${
+                  //     providerNameSuggestions[Math.floor(Math.random() * 4)]
+                  //   }` || providerData.name,
                   // provider_name:
                   //   `${user?.agent?.first_name}${providerNameSuggestions[0]}` ||
                   //   providerData.name,
+                  provider_name:
+                    `Detroit Public School's Battery` || providerData.name,
                   domain_id: domainId,
                   ...(imageId && { logo: imageId }),
                   ...(providerData.short_desc && {
