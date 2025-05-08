cfbd951b98c4105ade08c4d6c67b9412a6f4d401
aniketceminds <aniket.chahare@eminds.ai>
2025-05-08 20:11:26 +0530

    feat: added CURD apis for meter

---
+++ plugins/meter-data-simulator/server/routes/meter.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    method: "POST",
+    path: "/meters",
+    handler: "meter.create",
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: "PATCH",
+    path: "/meters/:id",
+    handler: "meter.update",
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: "GET",
+    path: "/meters",
+    handler: "meter.get",
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: "GET",
+    path: "/meters/:id",
+    handler: "meter.getById",
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/meters/:id",
+    handler: "meter.delete",
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+];
--- plugins/meter-data-simulator/server/services/index.ts
+++ plugins/meter-data-simulator/server/services/index.ts
@@ -1,5 +1,5 @@
-import myService from './my-service';
+import meter from './meter';
 
 export default {
-  myService,
+  meter,
 };
