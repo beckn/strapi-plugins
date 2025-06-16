a427359812c046008fb3ac73b1354573bcfe70ae
shreyvishal <shreya.vishal@eminds.ai>
2025-06-16 18:02:39 +0530

    Added: logs for debugging

--- plugins/registry/server/src/services/dedi.ts
+++ plugins/registry/server/src/services/dedi.ts
@@ -156,7 +156,7 @@ export default {
 
     const result = await response.json();
     strapi.log.info(`Response for login: ${JSON.stringify(result, null, 2)}`);
-    return result.message.access_token;
+    return result.message.data.access_token;
   },
 
   async _makeRequest(endpoint: string, method: string, body?: object): Promise<any> {
