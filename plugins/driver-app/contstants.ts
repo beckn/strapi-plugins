e887be04e1b18a36dd26a947900539db850c0d3f
shreyvishal <shreya.vishal@eminds.ai>
2025-10-06 18:19:24 +0530

    Added: Changes for mobility domain

--- plugins/driver-app/contstants.ts
+++ plugins/driver-app/contstants.ts
@@ -1,6 +1,10 @@
 import pluginPkg from "./package.json";
+import dotenv from "dotenv";
 export const PLUGIN: string = pluginPkg.strapi.name;
-export const MOBILITY_DOMAIN = "mobility:1.1.0";
+
+export const MOBILITY_DOMAIN = `mobility:1.1.0${
+  process.env.BECKN_ENV ? `:${process.env.BECKN_ENV}` : ""
+}`;
 export enum RIDE_STATUS_CODE {
   AWAITING_DRIVER_APPROVAL = "AWAITING_DRIVER_APPROVAL",
   RIDE_ACCEPTED = "RIDE_ACCEPTED",
